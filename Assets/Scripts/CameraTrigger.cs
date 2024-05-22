7636325197e95341fb05c5b966bc78cdf5ff879e
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-05-21 20:24:54 -0500

    Cámaras y modelo darren

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
+    public Transform myCamera;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("Trigger activated");
+            Camera.main.transform.position = myCamera.transform.position;
+            Camera.main.transform.rotation = myCamera.transform.rotation;
         }
     }
 }
