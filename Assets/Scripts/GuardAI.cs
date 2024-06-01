b73fa1fb7ffd19ae221ada835bc3b17a7c651145
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-06-01 14:52:08 -0500

    Adelantos AI

--- Assets/Scripts/GuardAI.cs
+++ Assets/Scripts/GuardAI.cs
@@ -9,55 +9,86 @@ public class GuardAI : MonoBehaviour
     public int currentTarget;
     [SerializeField]
     private NavMeshAgent agent;
+    private bool reverse;
+    private bool targetReached;
 
-    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-        //if (wayPoints.Count > 0)
-        //{
-        //    if (wayPoints[0]  != null)
-        //    {
-        //        currentTarget = wayPoints[0];
-        //        agent.SetDestination(currentTarget.position);
-        //    }
-        //}
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (currentTarget != null)
-        //{
-        //    float distance = Vector3.Distance(transform.position, currentTarget.position);
-
-        //    if (distance < 1f)
-        //    {
-        //        if (wayPoints[1] != null && currentTarget != wayPoints[1])
-        //        {
-        //            currentTarget = wayPoints[1];
-        //            agent.SetDestination(currentTarget.position);
-        //        }
-
-        //        else if (wayPoints[2] != null)
-        //        {
-        //            currentTarget = wayPoints[2];
-        //            agent.SetDestination(currentTarget.position);
-        //        }
-        //    }
-        //}
-
         if (wayPoints.Count > 0 && wayPoints[currentTarget] != null)
         {
             agent.SetDestination(wayPoints[currentTarget].position);
 
             float distance = Vector3.Distance(transform.position, wayPoints[currentTarget].position);
 
-            if (distance < 1f)
+            if (distance < 1f && targetReached is false)
             {
-                currentTarget++;
+                if (currentTarget is 0 || currentTarget == wayPoints.Count - 1)
+                {
+                    targetReached = true;
+                    StartCoroutine(WaitBeforemoving());
+                }
+
+                else
+                {
+                    if (reverse)
+                    {
+                        currentTarget--;
+
+                        if (currentTarget <= 0)
+                        {
+                            reverse = false;
+                            currentTarget = 0;
+                        }
+                    }
+
+                    else
+                    {
+                        currentTarget++;
+                    }
+                }
             }
         }
+    }
+
+    IEnumerator WaitBeforemoving()
+    {
+        if (currentTarget is 0)
+        {
+            yield return new WaitForSeconds(2f);
+        }
+
+        else if (currentTarget == wayPoints.Count - 1)
+        {
+            yield return new WaitForSeconds(2f);
+        }
+
+        if (reverse)
+        {
+            currentTarget--;
+
+            if (currentTarget is 0)
+            {
+                reverse = false;
+                currentTarget = 0;
+            }
+        }
+
+        else if (!reverse)
+        {
+            currentTarget++;
+
+            if (currentTarget == wayPoints.Count)
+            {
+                reverse = true;
+                currentTarget--;
+            }
+        }
+
+        targetReached = false;
     }
 }
