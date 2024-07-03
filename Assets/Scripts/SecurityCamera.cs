2176e4f83161e0c7ebde30917fdf6433fa8a4ef4
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-07-02 20:34:06 -0500

    Avances camaras

---
+++ Assets/Scripts/SecurityCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    public GameObject gameOverCutscene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameOverCutscene.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/VoiceOverTrigger.cs
+++ Assets/Scripts/VoiceOverTrigger.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,25 @@ using UnityEngine;
 public class VoiceOverTrigger : MonoBehaviour
 {
     public AudioClip clipToPlay;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = Camera.main.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = Camera.main.gameObject.AddComponent<AudioSource>();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag is "Player")
+        if (other.tag == "Player")
         {
-            AudioSource.PlayClipAtPoint(clipToPlay, Camera.main.transform.position);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(clipToPlay);
+            }
         }
     }
 }
