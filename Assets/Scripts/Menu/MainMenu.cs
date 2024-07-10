7cb206d0cffe51cc9c664693d3307e7a9d8aa13f
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-07-10 17:27:05 -0500

    Finalizado

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("LoadingScreen");
     }
 
     // Update is called once per frame
